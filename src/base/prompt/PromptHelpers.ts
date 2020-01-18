d11130bcac92418ff1dcc3ee20e0fafbc9df5438
HampusAdolfsson <Hampus.Adolfsson.5594@student.uu.se>
2020-01-18 16:54:43 +0100

    Add UserPrompt and related stuff

---
+++ src/base/prompt/PromptHelpers.ts
@@ -0,0 +1,43 @@
+import { DMChannel, GroupDMChannel, Message, TextChannel, User } from "discord.js";
+import { UserPrompt } from "./UserPrompt";
+
+// Some common prompts
+
+/**
+ * Prompts the user to select one of several menu options.
+ * Returns the index of the option chosen
+ */
+export class MenuPrompt extends UserPrompt<number> {
+    private options: string[];
+    public constructor(textPrompt: string,
+                       user: User,
+                       channel: TextChannel | DMChannel | GroupDMChannel,
+                       options: string[]) {
+        // Formats the options:
+        //   `1.` like
+        //   `2.` this
+        const prompt = textPrompt
+                       + "\n"
+                       + options.map((opt, i) => `  \`${i + 1}.\` ${opt}\n`)
+                       + "Please type in a number to select an option.";
+        super(prompt, user, channel);
+        this.options = options;
+    }
+
+    protected validate(message: Message): { valid: boolean; msg: string | undefined; } {
+        const matches = message.content.trim().match(/^(?<i>\d+)\.*$/);
+        if (!matches || !matches.groups)  {
+            return { valid: false, msg: "That is not a valid number." };
+        }
+        const chosenIndex = Number(matches.groups.i);
+        if (chosenIndex <= 0 || chosenIndex > this.options.length) {
+            return { valid: false, msg: "That number is out of range." };
+        }
+        return { valid: true, msg: undefined };
+    }
+
+    protected parse(message: Message): number {
+        const matches = message.content.trim().match(/^(?<i>\d+)\.*$/);
+        return Number(matches!.groups!.i);
+    }
+}
---
+++ src/base/prompt/UserPrompt.ts
@@ -0,0 +1,57 @@
+import { DMChannel, GroupDMChannel, Message, TextChannel, User } from "discord.js";
+import { Logger } from "../../Logger";
+
+/**
+ * Interactively prompts the user for some data.
+ */
+export abstract class UserPrompt<T> {
+    private static readonly CANCEL_STRING = "Respond with cancel to `cancel` the command. The command will automatically be cancelled in 30 seconds.";
+
+    /**
+     * Create a new prompt.
+     * @param textPrompt A prompt to show to the user (a mention will be added at the start)
+     * @param user The user to listen to
+     * @param channel The channel to prompt and listen in
+     */
+    public constructor(private textPrompt: string,
+                       private user: User,
+                       private channel: TextChannel | DMChannel | GroupDMChannel) {}
+
+    /**
+     * Run the prompt, interacting with the user until the prompt either gives a valid result, or is canceled.
+     */
+    public async run(): Promise<T> {
+        const collector = this.channel.createMessageCollector((m: Message) => m.author === this.user, { time: 30000 });
+
+        while (true) {
+            this.channel.send(`${this.user}, ${this.textPrompt}` + "\n" + UserPrompt.CANCEL_STRING);
+            try {
+                const msg = await collector.next;
+                Logger.Log(Logger.Severity.Debug, msg.content);
+
+                if (msg.content.trim().toLowerCase() === "cancel") {
+                    break;
+                }
+                const validation = this.validate(msg);
+                if (validation.valid) {
+                    collector.stop();
+                    return Promise.resolve(this.parse(msg));
+                } else {
+                    if (validation.msg) { this.channel.send(validation.msg); }
+                }
+            } catch (e) {
+                break; // probably timed out
+            }
+        }
+
+        Logger.Log(Logger.Severity.Debug, "Prompt canceled");
+        this.channel.send(`${this.user}, The command was canceled.`);
+        collector.stop();
+        return Promise.reject();
+    }
+
+    protected abstract validate(message: Message): { valid: boolean, msg: string | undefined };
+
+    protected abstract parse(message: Message): T;
+
+}
