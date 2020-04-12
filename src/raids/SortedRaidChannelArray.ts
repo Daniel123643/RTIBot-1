f603adce2b4ccd51b448597631f56ce3bef47827
Hampus <15944368+HampusAdolfsson@users.noreply.github.com>
2020-04-12 05:19:03 +0200

    Fix sorting issue in schedule

--- src/raids/SortedRaidChannelArray.ts
+++ src/raids/SortedRaidChannelArray.ts
@@ -23,14 +23,17 @@ export class SortedRaidChannelArray {
         return this.items;
     }
 
+    /**
+     * Add the given raid channel to the array
+     * @param channel The channel to add
+     */
     public add(channel: RaidEventChannel) {
-        const i = this.prospectiveIndexOf(channel.event);
-
-        this.items.splice(i, 0, channel);
+        this.items.push(channel);
+        this.items = this.items.sort((ch1, ch2) => this.compareEvents(ch1.event, ch2.event));
     }
 
     /**
-     * Removed the given raid channel from the array
+     * Remove the given raid channel from the array
      * @param event The channel to remove
      * @returns Whether something was removed
      */
@@ -42,23 +45,11 @@ export class SortedRaidChannelArray {
         return index >= 0;
     }
 
-    public prospectiveIndexOf(event: RaidEvent): number {
-        if (this.items.length === 0) {
-            return 0;
-        }
-
-        let i = this.items.length;
-        while (i > 0 && this.compareEvents(this.items[i - 1].event, event) >= 0) {
-            i--;
-        }
-        return i;
-    }
-
     private compareEvents(ev1: RaidEvent, ev2: RaidEvent): number {
-        if (ev1.startTime.isSame(ev2.startTime)) {
+        if (!ev1.startTime.isSame(ev2.startTime)) {
             return ev1.startTime.isBefore(ev2.startTime) ? -1 : 1;
         }
-        if (ev1.endTime.isSame(ev2.endTime)) {
+        if (!ev1.endTime.isSame(ev2.endTime)) {
             return ev1.endTime.isBefore(ev2.endTime) ? -1 : 1;
         }
         return -1;
