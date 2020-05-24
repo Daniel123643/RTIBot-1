08e59c93762f0751c86ea75bf457b7a4757cc3bd
Hampus Adolfsson <hampus@adolfsson.email>
2020-05-24 20:43:28 +0200

    Show available slots when registering

--- src/raids/RaidRegistrationDialog.ts
+++ src/raids/RaidRegistrationDialog.ts
@@ -14,11 +14,11 @@ export class RaidRegistrationDialog extends UserDialog<RaidRole> {
     protected async doExecute(): Promise<RaidRole> {
         await this.say("You are registering for the raid \"" + this.event.name + "\"");
 
-        const roleNames = this.event.roles.map(r => r.name);
+        const roleStrings = this.event.roles.map(r => `${r.name} (${r.numActiveParticipants}/${r.numRequiredParticipants})`);
         const roleIndex = await new MenuPrompt("What role do you want to register as?",
                                                 this.user,
                                                 this.channel,
-                                                roleNames).run();
+                                                roleStrings).run();
 
         await this.say("You have been registered for the raid!");
         const selectedRole = this.event.roles[roleIndex];
